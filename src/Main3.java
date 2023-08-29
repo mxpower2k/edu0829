7d9776aef8668366f09c9b6e90f593321041972a
hansookim <suekhan@naver.com>
2023-08-29 17:07:37 +0900

    main41 함수 생성

--- src/Main3.java
+++ src/Main3.java
@@ -5,4 +5,9 @@ public class Main3 {
     public void main4() {
         System.out.println("main4 함수 추가");
     }
+
+    public void main41()
+    {
+        System.out.println("main41 함수추가");
+    }
 }
