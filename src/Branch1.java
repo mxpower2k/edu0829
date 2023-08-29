551ed0ad6614da0ffb4b0226b733b3db112a51a5
hansookim <suekhan@naver.com>
2023-08-29 15:50:06 +0900

    test2 함수추가

--- src/Branch1.java
+++ src/Branch1.java
@@ -2,4 +2,8 @@ public class Branch1 {
     public void test1() {
         System.out.println("test1 함수 추가");
     }
+
+    public void test2() {
+        System.out.println("test2 함수 추가");
+    }
 }
