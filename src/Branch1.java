07f193a4bf906abbebd0532e4008df9c4a89c1b8
hansookim <suekhan@naver.com>
2023-08-30 10:24:11 +0900

    test5 함수추가

--- src/Branch1.java
+++ src/Branch1.java
@@ -6,4 +6,16 @@ public class Branch1 {
     public void test2() {
         System.out.println("test2 함수 추가");
     }
+
+    public void test3() {
+        System.out.println("test3 함수 추가");
+    }
+
+    public void test4() {
+        System.out.println("test4 함수추가");
+    }
+
+    public void test5() {
+        System.out.println("test5 함수추가");
+    }
 }
